fccacd9f7c3b303d4a49e0b63eb565e5bcb6424f
Diego Sano <diego.sano@nave.rs>
2022-06-15 16:16:17 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { hot } from 'react-hot-loader/root'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default hot(App)
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const config = {
+  entry: ['react-hot-loader/patch', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: './dist',
+    },
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'src/index.html' }],
+    }),
+    new CleanWebpackPlugin(),
+  ],
+}
+
+module.exports = config
