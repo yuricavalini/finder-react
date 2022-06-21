e26f770f49beef8a2924aeafddf19f2de23df5d7
Diego Sano <diego.sano@nave.rs>
2022-06-20 21:27:58 -0300

    feat: add dotenv-webpack plugin and create .env.example file

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios'
 
-import { API_URL } from '../helpers/constants'
-
 const defaultOptions = {
-  baseURL: API_URL,
+  baseURL: process.env.API_URL,
 }
 
 export const api = axios.create(defaultOptions)
