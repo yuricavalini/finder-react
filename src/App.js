8e03fb4fcfa48a7fe8a2ef5e4a59766e23da0f93
Diego Sano <diego.sano@nave.rs>
2022-06-20 21:10:58 -0300

    feat: add hotjar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { hot } from 'react-hot-loader/root'
 import { ThemeProvider } from 'styled-components'
+// import { hotjar } from 'react-hotjar'
 
 import AppRoutes from './routes/Routes'
 import { GlobalStyle } from './themes/GlobalStyle'
 import { theme } from './themes/theme'
+
+// hotjar.initialize(hjid, hjsv)
+// hotjar.identify('USER_ID', { userProperty: 'value' })
+// hotjar.event('button-click')
+// hotjar.stateChange('/my/page')
 
 const App = () => {
   return (
