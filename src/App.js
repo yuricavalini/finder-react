c0b4ac6f11f2289d7db16ffca7c5c9b100f0d7ea
Diego Sano <diego.sano@nave.rs>
2022-06-20 21:49:17 -0300

    feat: add and initialize gtm

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import { hot } from 'react-hot-loader/root'
 import { ThemeProvider } from 'styled-components'
 // import { hotjar } from 'react-hotjar'
+// import TagManager from 'react-gtm-module'
 
 import AppRoutes from './routes/Routes'
 import { GlobalStyle } from './themes/GlobalStyle'
 import { theme } from './themes/theme'
 
-// hotjar.initialize(hjid, hjsv)
-// hotjar.identify('USER_ID', { userProperty: 'value' })
+// hotjar.initialize(process.env.HOTJAR_HJID, process.env.HOTJAR_HJSV)
+// hotjar.identify(process.env.HOTJAR_USER_ID, { userProperty: 'value' })
 // hotjar.event('button-click')
 // hotjar.stateChange('/my/page')
+
+// const tagManagerArgs = {
+//   gtmId: process.env.GTM_ID,
+// }
+
+// TagManager.initialize(tagManagerArgs)
 
 const App = () => {
   return (
